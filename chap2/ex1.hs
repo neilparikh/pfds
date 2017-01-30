eeda7f1e869e991ea8077e0526f791435a7b78d5
Neil Parikh <parikh.neil@me.com>
2017-01-29 22:36:22 -0500

    add test for ex2.1

--- chap2/ex1.hs
+++ chap2/ex1.hs
@@ -1,4 +1,12 @@
+import Data.List (tails)
+import Test.QuickCheck
+
+main = quickCheck prop_suffixes_eq_tails
+
 -- Ex 2.1
 suffixes :: [a] -> [[a]]
 suffixes [] = []:[]
 suffixes list@(_:xs) = list:suffixes xs
+
+prop_suffixes_eq_tails :: [Int] -> Bool
+prop_suffixes_eq_tails list = suffixes list == tails list
