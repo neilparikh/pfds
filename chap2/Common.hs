b980641fd0893d022c28657ecca796827e29eb3d
Neil Parikh <parikh.neil@me.com>
2017-01-29 22:50:22 -0500

    add test for ex2.2

--- chap2/Common.hs
+++ chap2/Common.hs
@@ -1,6 +1,20 @@
 module Common where
+import Test.QuickCheck (Arbitrary, arbitrary, oneof)
 
 data Tree a = E | T (Tree a) a (Tree a) deriving Show
+
+instance (Arbitrary a) => Arbitrary (Tree a) where
+    arbitrary = oneof [
+        pure E,
+        T <$> arbitrary <*> arbitrary <*> arbitrary
+        ]
+
+member :: Ord a => Tree a -> a -> Bool
+member E _ = False
+member (T left y right) x
+    | x < y = member left x
+    | x > y = member right x
+    | otherwise = True
 
 insert :: (Ord a) => Tree a -> a -> Tree a
 insert E x = T E x E
--- chap2/ex2.hs
+++ chap2/ex2.hs
@@ -1,8 +1,17 @@
-import Common
+import qualified Common
+import Common (Tree(..))
+import Test.QuickCheck
+
+main = quickCheck prop_member_should_match_reference_impl
+
 -- Ex 2.2
+
 member :: Ord a => Tree a -> a -> Maybe a -> Bool
 member E _ Nothing = False
 member E x (Just possible) = x == possible
 member (T left y right) x maybePossible
     | x < y = member left x maybePossible
     | otherwise = member right x (Just y)
+
+prop_member_should_match_reference_impl :: Tree Int -> Int -> Bool
+prop_member_should_match_reference_impl tree x = member tree x Nothing == Common.member tree x
