1e28820d547bc41b718482b4268bdb6c599e2d0d
Neil Parikh <parikh.neil@me.com>
2017-01-29 22:54:29 -0500

    add test for ex2.4

--- chap2/ex4.hs
+++ chap2/ex4.hs
@@ -1,4 +1,9 @@
-import Common
+import qualified Common
+import Common (Tree(..))
+import Test.QuickCheck
+
+main = quickCheck prop_insert_should_match_reference_impl
+
 -- Ex 2.4
 insert :: (Ord a) => Tree a -> a -> Tree a
 insert tree x = case (insert' tree Nothing) of
@@ -12,3 +17,6 @@ insert tree x = case (insert' tree Nothing) of
     insert' (T left y right) possibleMatch
         | x < y = T <$> (insert' left possibleMatch) <*> (pure y) <*> (pure right)
         | otherwise = T left y <$> (insert' right (Just y))
+
+prop_insert_should_match_reference_impl :: Tree Int -> Int -> Bool
+prop_insert_should_match_reference_impl tree x = insert tree x == Common.insert tree x
