b660a4d8c316c9051dde9e98b6aa2680891c6312
Neil Parikh <parikh.neil@me.com>
2017-07-15 20:51:39 -0400

    change arbitrary instance to one that only produces BSTs

--- chap2/Common.hs
+++ chap2/Common.hs
@@ -1,13 +1,20 @@
 module Common where
-import Test.QuickCheck (Arbitrary, arbitrary, oneof)
+import Test.QuickCheck (Arbitrary, arbitrary, frequency, choose, Gen)
+import System.Random (Random)
+import Control.Monad (liftM3)
 
 data Tree a = E | T (Tree a) a (Tree a) deriving (Show, Eq)
 
-instance (Arbitrary a) => Arbitrary (Tree a) where
-    arbitrary = oneof [
-        pure E,
-        T <$> arbitrary <*> arbitrary <*> arbitrary
-        ]
+-- from http://www.seas.upenn.edu/~cis552/12fa/lectures/stub/BST.html
+instance (Ord a, Bounded a, Random a, Num a, Arbitrary a) => Arbitrary (Tree a)  where
+    arbitrary = gen 0 100 where
+        gen :: (Ord a, Num a, Random a) => a -> a -> Gen (Tree a)
+        gen min max | (max - min) <= 3 = return E
+        gen min max = do
+            elt <- choose (min, max)
+            frequency [ (1, return E),
+                        (6, liftM3 T (gen min (elt - 1))
+                        (return elt) (gen (elt + 1) max)) ]
 
 member :: Ord a => Tree a -> a -> Bool
 member E _ = False
